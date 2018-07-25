66a2ef7679027af9ae2b3885b34373b4cbe0702b
eagle <bluecotton.k@gmail.com>
2018-07-26 06:43:16 +0700

    การใช้ตัวแปร

--- hexagon.py
+++ hexagon.py
@@ -3,9 +3,21 @@ from turtle import *
 def simple_hexagon():
     for i in range(6):
         forward(100)
-        left(60)
+        left(360 / 6)
+
+def simple_hexagon_2():
+    '''
+    สร้างตัวแปรสำหรับเก็บจำนวนด้าน
+    '''
+    side = 6
+    for i in range(side):
+        forward(100)
+        left(360 / side)
 
 def polygon(side):
+    '''
+    สร้างเป็นฟังก์ชัน
+    '''
     for i in range(side):
         forward(100)
         left(360 / side)
